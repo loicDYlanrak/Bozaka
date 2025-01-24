961897ac9ac59341e6d9c2bff1d476ccde92a178
loicDYlanrak <loicrakotoarivony07@gmail.com>
2025-01-24 14:39:37 +0300

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,11 +1,5 @@
 <?php
 
-use app\controllers\ApiExampleController;
-
-use app\controllers\UserController;
-use app\controllers\PropertyController;
-use app\controllers\HomeController;
-use app\controllers\AdminController;
 
 use flight\Engine;
 use flight\net\Router;
@@ -19,38 +13,6 @@ use flight\net\Router;
 	$Welcome_Controller = new WelcomeController($app);
 	$app->render('welcome', [ 'message' => 'It works!!' ]);
 });*/
-
-$HomeController = new HomeController();
-$UserController = new UserController();
-$AdminController = new AdminController();
-$router->get('/', [$UserController, 'accueil']);
-$router->get('/register', [$UserController, 'goInscription']);
-$router->get('/login', [$UserController, 'showLoginForm']);
-$router->get('/deconnexion', [$UserController, 'logout']);
-
-$router->post('/login', [$UserController, 'login']);
-$router->post('/register', [$UserController, 'register']);
-
-$router->get('/ajoutHabitation', [$AdminController,'goAjoutHabitation']);
-$router->post('/ajoutHabitation', [$AdminController,'ajoutHabitation']);
-
-$router->get('/loginAdmin', [$AdminController,'goLoginAdmin']);
-$router->post('/loginAdmin', [$AdminController,'connectAdmin']);
-
-$router->get('/modifier/@id', [$PropertyController, 'goModifierHabitation']);
-$router->post('/modifier', [$PropertyController, 'modifierHabitation']);
-
-$router->get('/supprimer/@id', [$PropertyController, 'supprimerHabitation']);
-$router->get('/updateHabitation', [$AdminController,'goUpdateHabitation']);
-$router->post('/updateHabitation', [$AdminController,'updateHabitation']);
-
-
-$PropertyController = new PropertyController();
-$router->get('/accueil', [$PropertyController, 'getAllProperty']);
-$router->get('/detail/@id', [$PropertyController, 'detailProperty']);
-$router->post('/reserve', [$PropertyController, 'reserveProperty']);
-$router->get('/search', [$PropertyController, 'searchProperty']);
-$router->get('/reservations', [$PropertyController, 'showReservations']);
 
 
 // $AdminController = new AdminController();
